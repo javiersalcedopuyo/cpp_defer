9de6f9776e41f53f714f1129eea580fe98501f49
Javier Salcedo <javiersalcedo.p@gmail.com>
2024-10-09 22:10:38 +0100

    Add implementation

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # cpp_defer
-Simple defer implementation for C++ 11 and newer.
+Simple single header `defer` implementation for C++ 11 and newer, inspired by languages like Zig and Swift.
+
+## Usage example
+
+```c++
+#include <cstdio>
+#include "defer.hpp"
+
+// I personally like to redefine the macro to something that looks more like an
+// actual language feature.
+#define defer(f) JSP_DEFER(f)
+
+int main()
+{
+    defer( printf("Outer\n") );
+    {
+        defer( printf("Inner\n") );
+        defer( printf("Inner 2\n") );
+    }
+    return 0;
+}
+```
+
+Output:
+```
+Inner 2
+Inner
+Outer
+```
---
+++ defer.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <functional>
+
+namespace jsp
+{
+    class Deferrable
+    {
+    public:
+        Deferrable() = delete;
+        Deferrable( std::function<void()> _f ): f( _f ) {}
+        ~Deferrable() { f(); }
+    private:
+        const std::function<void()> f;
+    };
+}
+
+// Preprocessor magic so __COUNTER__ can be expanded correctly
+#define JSP_CONCATENATE( l, r )         JSP_DO_CONCATENATE( l, r )
+#define JSP_DO_CONCATENATE( l, r )      JSP_DO_CONCATENATE_2( l, r )
+#define JSP_DO_CONCATENATE_2( l, r )    l##r
+
+#define JSP_DEFER(f) const auto JSP_CONCATENATE(_deferred, __COUNTER__) = jsp::Deferrable( [](){ f; } );
